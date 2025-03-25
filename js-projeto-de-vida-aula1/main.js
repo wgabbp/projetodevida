539c8d795245f7c1294a5ab35fe159db7881b768
wgabbp <144801367+wgabbp@users.noreply.github.com>
2025-03-25 10:11:36 -0300

    projeto.5

--- js-projeto-de-vida-aula1/main.js
+++ js-projeto-de-vida-aula1/main.js
@@ -22,7 +22,7 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -30,7 +30,7 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+     return dias+ "dias" + horas"+ horas " + minutos + "minutos" +segundos +"segundos"
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
